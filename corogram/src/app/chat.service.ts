89567bf5e01c3ad34a8cf4c7c3616ebedc80eb9e
robincuenoud <robin.cuenoud@heig-vd.ch>
2020-03-19 18:44:36 +0100

    ajout de la case pour envoyer des messages

--- corogram/src/app/chat.service.ts
+++ corogram/src/app/chat.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ChatMessage } from './chat.message';
 import {CHAT_MESSAGES} from './mock-chat';
+
+
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
+
+
 @Injectable({
   providedIn: 'root'
 })
