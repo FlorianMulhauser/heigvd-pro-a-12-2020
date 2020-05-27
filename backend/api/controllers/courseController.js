22004c80119959388688433ff5f835a3ef882520
robincuenoud <48149097+robincuenoud@users.noreply.github.com>
2020-05-27 12:14:44 +0200

    rights almost done in course

--- backend/api/controllers/courseController.js
+++ backend/api/controllers/courseController.js
@@ -2,13 +2,14 @@
 
 var mongoose = require('mongoose'),
 Course = mongoose.model('Course'); // pour les cours
+var rH = require('./rightHelper');
 
 exports.list_all_course = function(req, res) {
   Course.find({}, function(err, course) {
     if (err)
       res.send(err);
 
-    if(req.user.status == "admin" || req.user.status == "superadmin") {
+    if(rH.isAdmin(req)) {
       res.json(course);
     } else {
       // only sends course that user has
@@ -23,17 +24,16 @@ exports.list_all_course = function(req, res) {
 
 
 exports.create_a_course = function(req, res) {
-
+  if(rH.isAdmin(req)) {
   var new_course = new Course(req.body);
   new_course.save(function(err, course) {
     if (err)
       res.send(err);
-    if(req.user.status == "admin" || req.user.status == "superadmin") {
     res.json(course);
-  } else { // pas authorizé à le faire
-    res.sendStatus(403);
-  }
   });
+} else { //if not admin
+  res.sendStatus(403);
+}
 };
 
 
@@ -41,22 +41,30 @@ exports.read_a_course = function(req, res) {
   Course.findById(req.params.courseId, function(err, course) {
     if (err)
       res.send(err);
-    res.json(course);
+    if(rH.isAdmin(req) || -1 != req.user.course.indexOf(req.params.courseId)) {
+      res.json(course);
+    } else {
+      res.sendStatus(403);
+    }
   });
 };
 
 
 exports.update_a_course = function(req, res) {
+  if(rH.isAdmin(req)) {
   Course.findOneAndUpdate({_id: req.params.courseId}, req.body, {new: true}, function(err, course) {
     if (err)
       res.send(err);
     res.json(course);
   });
+} else {
+  res.sendStatus(403);
+}
 };
 
 
 exports.delete_a_course = function(req, res) {
-  if(req.user.status == "admin" || req.user.status == "superadmin") {
+  if(rH.isAdmin(req)) {
   Course.remove({
     _id: req.params.courseId
   }, function(err, course) {
