3f9f2f39f7558c6f0dee8bd4a2b5d5c92e83e0eb
yoannsim <yoann.simonet@heig-vd.ch>
2020-05-29 06:11:12 +0200

    rm user inutile aff

--- corogram/src/app/home/home.component.ts
+++ corogram/src/app/home/home.component.ts
@@ -1,14 +1,21 @@
 import {Component} from '@angular/core';
 import {Course} from '../_service/course/course';
+import {ForumMessage} from '../_service/forum/forum.message';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
   public selectedCourse: Course;
+  public statu: boolean;
+
+
+  public ngOnInit(): void {
+    this.statu = JSON.parse(localStorage.getItem('userInfo')).status  !== 'user';
+  }
 
   public selectCourse(course: Course) {
     this.selectedCourse = course;
